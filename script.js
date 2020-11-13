87709c1ec2d533fefc3b15626ae119e8a9e8d953
Caspento <Mrcaspento@gmail.com>
2020-11-13 13:29:27 -0500

    new branch

---
+++ script.js
@@ -0,0 +1,21 @@
+var apiKey= "wl3TeYSBuwDsDxyW8rc3TGrXdcEzt7BM";
+var userSearch = $('_______').val().trim();
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearcht + "&api-key=" + apiKey
+var articleNumber = $('________').val();;
+
+
+
+;
+$.ajax({
+url: queryURL,
+method: "GET"
+}).then(function(response){
+console.log(response)    
+
+for(var i = 0; i > _____; i++ )
+var searchDiv 
+var 
+})
+
+
+
